5a9bb7310b1447560fa53bc0ef2c4d4790cb31c2
xuchao <xuchao17@baidu.com>
2017-08-02 20:59:55 +0800

    Initial Commit

---
+++ MyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCollectionView
+//
+//  Created by Xu,Chao(MMS) on 2017/8/2.
+//  Copyright © 2017年 Xu,Chao(MMS). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
